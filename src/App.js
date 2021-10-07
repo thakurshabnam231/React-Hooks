408e66d2433eae7411e46761b6cc76cafd8c9ccb
shabnam rani <88382451+thakurshabnam231@users.noreply.github.com>
2021-10-06 22:23:44 -0700

    counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import UseEffect from './Components/UseEffect';
 import UseEffectCleanUp from './Components/UseEffect';
+import UseEffectCounter from './Components/UseEffectCounter';
 import UseStateArray from './Components/UseStateArray';
 import UseStateCounter from './Components/UseStateCounter';
 import UseStateObj from './Components/UseStateObj';
@@ -8,6 +9,7 @@ import UseStateObj from './Components/UseStateObj';
 function App() {
   return (
     <div className="App">
+    <UseEffectCounter/>
     <UseStateArray/>
     <UseStateObj/>
     <UseStateCounter/>
---
+++ src/Components/UseEffectCounter.js
@@ -0,0 +1,22 @@
+import React,{useState,useEffect} from 'react'
+
+const UseEffectCounter = () => {
+    const[count,setCount]=useState(0);
+
+    const tick=()=>{
+        setCount(prevCount=>prevCount+1);
+    }
+    useEffect(()=>{
+        const interval=setInterval(tick,1000) 
+       return()=>{
+       clearInterval(interval)
+   }
+    },[])
+    return (
+        <div>
+            <h1>{count}</h1>
+        </div>
+    )
+}
+
+export default UseEffectCounter
